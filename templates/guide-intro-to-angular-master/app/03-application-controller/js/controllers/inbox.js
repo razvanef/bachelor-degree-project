128b0d9317678ae789b97b06213754225f3305b0
razvan.florescu <razvan.florescu@dpdgroup.co.uk>
2018-06-18 18:29:38 +0300

    delete files

--- templates/guide-intro-to-angular-master/app/03-application-controller/js/controllers/inbox.js
+++
@@ -1,9 +0,0 @@
-/**
- * Controller: InboxCtrl
- */
-angular.module('EmailApp')
-  .controller('InboxCtrl',
-    function InboxCtrl ( ) {
-      'use strict';
-      console.log('Inbox Loaded!')
-    });
--- templates/singup/js/index.js
+++
@@ -1,9 +0,0 @@
-$(".close, .nope").on('click', function () {
-  $('.modal').addClass('hidden');
-  $('.open').addClass('active');
-})
-
-$(".open").on('click', function () {
-  $(this).removeClass('active');
-  $('.modal').removeClass('hidden');
-})
